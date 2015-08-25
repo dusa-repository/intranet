08c26dcb6064ac91c81558130f080a0ced13083c
willi <linkin9014@gmail.com>
2015-08-25 18:59:24 -0400

    Prueba de estilo

--- bizzaps-intranet-dusa-new/src/controlador/portal/CNoticiaPortal.java
+++ bizzaps-intranet-dusa-new/src/controlador/portal/CNoticiaPortal.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.stereotype.Controller;
 import org.zkoss.zhtml.H1;
+import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Label;
 
@@ -19,8 +20,6 @@ public class CNoticiaPortal extends CGenerico {
 
 	@Override
 	public void inicializar() throws IOException {
- 
-		titulo.setValue("nknjvdfvhdfughsiñghdsñighdfsoibh");
 		noticia.setValue("nknjvdfv-------NOTICIA-----------hdfsoibh");
 		
 	}
